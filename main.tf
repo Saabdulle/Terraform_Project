08198c1220c60b5f2b7a5698ea7a56704157bf09
Saeed Abdulle <104086456+Saabdulle@users.noreply.github.com>
2025-11-13 18:26:36 +0000

    added security module and updated main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,19 @@ module "Apps-Server" {
   server_count      = var.server_count
   public_subnets    = module.Networking.public_subnets_ids
 }
+module "Networking" {
+  source             = "./Modules/Networking"
+  public_subnets     = var.public_subnets
+  private_subnets    = var.private_subnets
+  availability_zones = var.availability_zones
+  cidr_block         = var.cidr_block
+}
+module "DynamoDB" {
+  source      = "./Modules/DynamoDB"
+  table_tags  = var.table_tags
+  table_names = var.table_names
+}
+module "Security" {
+  source = "./Modules/Security"
+  vpc_id = module.Networking.vpc_id
+}
